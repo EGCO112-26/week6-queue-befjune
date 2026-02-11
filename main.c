f56ea2c7a8ca50b690159b650fb946f1e89200fa
befjune <may.renuka.ch@gmail.com>
2026-02-11 08:44:48 +0000

    first assignment

--- main.c
+++ main.c
@@ -7,23 +7,26 @@
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
+  NodePtr tailPtr=NULL;
+
+// For struct Queue
   Queue  q;
    q. headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
-*/
+
    int i,x;
    
 
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
+            x=dequeue_struct(&q);
+            //x=dequeue(&headPtr,&tailPtr);
             printf("dequeing %d\n",x);
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+          //enqueue(&headPtr,&tailPtr, atoi(argv[i]));
+          enqueue_struct(&q, atoi(argv[i]));
            
         }
  }
