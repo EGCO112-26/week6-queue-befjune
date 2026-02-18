4b67664c2c50040db9244b18a7517619f5dbd94a
befjune <may.renuka.ch@gmail.com>
2026-02-18 23:39:34 +0700

    .h file for extra credit assignment

---
+++ Store.h
@@ -0,0 +1,21 @@
+
+#ifndef Store_h
+#define Store_h
+
+void intro()
+{
+    printf("\n-------- MENU --------\n");
+    printf("1 Yogurt : 40 Baht\n");
+    printf("2 Greek Yogurt : 50 Baht\n");
+    printf("3 Strawberry Milk : 30 Baht\n");
+}
+
+void howto()
+{
+    printf("\n-------- HOW TO ORDER --------\n");
+    printf("1.Choose your menu\n");
+    printf("2.Type menu-num amount x ; 1 3 x\n");
+    printf("3.Enter cash\n");
+}
+
+#endif
